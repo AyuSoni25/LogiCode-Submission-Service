70ffe73426021bb75c4b87274782766b3e0e1e27
Ayushi Soni <ayushisoni@Ayushis-MacBook-Air.local>
2024-08-23 22:43:18 +0530

    Feat: Added sync communication between microservices

--- src/services/submissionService.js
+++ src/services/submissionService.js
@@ -1,3 +1,4 @@
+const fetchProblemDetails = require("../apis/problemAdminApi");
 const SubmissionCreationError = require("../errors/submissionCreationError");
 const submissionQueueProducer = require("../producers/submissionQueueProducer")
 
@@ -11,11 +12,30 @@ class SubmissionService {
     }
 
     async addSubmission(submissionPayload) {
+        // hit the problem admin service and fetch the problem details
+        const problemId = submissionPayload.problemId;
+        const problemAdminApiResponse = await fetchProblemDetails(problemId);
+        
+        if(!problemAdminApiResponse){
+            throw new SubmissionCreationError('Failed to create a submission')
+        }
+
+        const languageCodeStub = problemAdminApiResponse.data.codeStubs.find(codeStub => codeStub.language.toLowerCase() === submissionPayload.language.toLowerCase());
+        submissionPayload.code = languageCodeStub.startSnippet + '\n\n' + submissionPayload.code + '\n\n' + languageCodeStub.endSnippet;
+
+        // we are going to create the entry in db
         const submission = await this.submissionRepository.createSubmission(submissionPayload);
         if(!submission){
             throw new SubmissionCreationError('Failed to create a submission')
         }
-        const response = await submissionQueueProducer(submission);
+        const response = await submissionQueueProducer({
+            [submission._id]: {
+            code: submission.code,
+            language: submission.language,
+            inputCase: problemAdminApiResponse.data.testCases[0].input,
+            outputCase: problemAdminApiResponse.data.testCases[0].output
+            }
+        });
         return {queueResponse: response, submission};
     }
 }
